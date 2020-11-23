b7150c4a6a51c98968e3cdfa9205eee7754c8bf7
bruno pessoa <brunopessoa.melo@gmail.com>
2020-11-23 17:14:35 -0300

    codificando o front-end do componente SearchBar.js

---
+++ meuapp/src/components/Cabecalho/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../styles/scss/button.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+
+const SearcBar = () => {
+    return(
+        <>
+            <div className="header__search">
+                <input type="text" placeholder="O você procura?" />
+                <button className="btn btn-search"><FontAwesomeIcon icon={faSearch} /></button>
+            </div>
+        </>
+    )
+}
+
+export default SearcBar;
