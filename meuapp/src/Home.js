3e75339f6ec1b7db84a905a61bec96c17dddf4c3
bruno pessoa <brunopessoa.melo@gmail.com>
2020-11-22 21:53:07 -0300

    aplicando o font-end no componente Banner

---
+++ meuapp/src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Cabecalho/Header';
+import Footer from './components/Footer/Footer';
+import Banner from './components/Banner/Banner';
+import Produto from './components/Produto/Produto';
+import './styles/scss/home.scss';
+
+const Home = () => {
+    return(
+        <>
+            <Header />
+            <Banner />
+            <Produto />
+            <Produto />
+        </>
+    )
+}
+
+export default Home;
---
+++ meuapp/src/components/Banner/Banner.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Slider from  "react-slick";
+import banner from '../../imagens/Fullbanner.jpg';
+
+const Banner = () =>{
+    const configuracoes = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
+    return(
+        <>
+            <section>
+                <div className="slider">
+                    <Slider {...configuracoes}>
+                        <div className="box-banner">
+                            <img
+                                width="1922"
+                                height="430"
+                                alt="banner 1"
+                                src="{banner}"
+                            />
+                        </div>
+
+                        <div className="box-banner">
+                            <img
+                                width="1920" 
+                                height="430"
+                                alt="banner 1"
+                                src="https://via.placeholder.com/1920x430/269f94?text=Banner"
+                            />
+                        </div>
+
+                        <div className="box-banner">
+                            <img 
+                                width="1920"
+                                height="430"
+                                alt="banner 2"
+                                src="https://via.placeholder.com/1920x430/269f94?text=Banner"
+                            />
+                        </div>
+
+                       <div className="box-banner">
+                            <img 
+                                width="1920"
+                                height="430"
+                                alt="banner 2"
+                                src="https://via.placeholder.com/1920x430/269f94?text=Banner"
+                            />
+                        </div> 
+                    </Slider>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Banner;
